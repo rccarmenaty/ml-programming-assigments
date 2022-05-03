6e2f9fc28d1aac0a6dccff12ad95a4548e0a4cc2
Ramon Carmenaty <ramoncarmenaty@gmail.com>
2022-05-03 12:08:17 -0400

    gradient descent

--- gradientDescent.m
+++ gradientDescent.m
@@ -16,9 +16,21 @@ for iter = 1:num_iters
     % Hint: While debugging, it can be useful to print out the values
     %       of the cost function (computeCost) and gradient here.
     %
-
-
-
+    
+    %number of features
+    [n, c] = size(theta);
+    
+      
+      derivative = (X * theta) - y; % * X(i,j)
+      for j = 1:n
+          X(:,j);
+          vectorj = X(:,j);
+          product = vectorj .* derivative;
+          theta(j) = theta(j) - (alpha / m) * sum(product);   
+      endfor
+    
+    
+    
 
 
 
