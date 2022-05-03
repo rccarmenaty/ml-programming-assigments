5414ab9a08d6d118e05e09fee57e12c8df7716a8
Ramon Carmenaty <ramoncarmenaty@gmail.com>
2022-05-03 10:08:59 -0400

    compute cost

--- computeCost.m
+++ computeCost.m
@@ -13,7 +13,10 @@ J = 0;
 % Instructions: Compute the cost of a particular choice of theta
 %               You should set J to the cost.
 
+hypothesys = X * theta;
+error = (hypothesys - y) .^ 2;
 
+J = sum(error) / (2 * m);
 
 
 
